865f3f5574f9d24286f9f8cc57d87b67bd21878b
elicul <enzo.licul@yahoo.com>
2018-10-15 14:53:58 +0200

    Auth module removed

--- Raspberry/server/src/app.module.ts
+++ Raspberry/server/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationModule } from './location/location.module';
 import { TemperatureModule } from './temperature/temperature.module';
@@ -7,7 +6,6 @@ import { TemperatureModule } from './temperature/temperature.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    // AuthModule,
     LocationModule,
     TemperatureModule,
   ],
